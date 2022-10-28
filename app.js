a2b574c549bebb9d054774590c1952e230d91803
amaraa <moonkeymon924@gmail.com>
2022-10-28 08:09:37 +0800

    finance modul hiij ehlsen expence income odjectuudiig uusgelee

--- app.js
+++ app.js
@@ -1,25 +1,84 @@
 //дэлгэцтэй ажиллах controller
-var uiController = (function () {})();
+var uiController = (function () {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__ddescription",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
+  };
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+  };
+})();
 
 //санхүүтай ажиллах controller
 var financeController = (function () {
-  //1. oruulah ugugdliig delgetsees olj awna
-  //2.olj awsan ugugluudiig sanhuugiin controllert damjuulj hadgalna
-  //3.olj awsan ogogdluude web deere tohiroh hesegt n gargana
-  //4.tuswiig tootsoolno
-  //5.etssiin vldegdel tootsoog delgetsend gargana
+  var Income = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var Expence = function (id, description, value) {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+
+  var data = {
+    allItems: {
+      inc: [],
+      exp: [],
+    },
+
+    totals: {
+      inc: 0,
+      exp: 0,
+    },
+  };
 })();
 
 //програмын холбогч controller
 var appController = (function (uiController, financeController) {
-  var ctrlAddItem = function () {};
-  document.querySelector(".add__btn").addEventListener("click", function () {
-    ctrlAddItem();
-  });
+  var ctrlAddItem = function () {
+    //1. oruulah ugugdliig delgetsees olj awna
+    //2.olj awsan ugugluudiig sanhuugiin controllert damjuulj hadgalna
+    //3.olj awsan ogogdluude web deere tohiroh hesegt n gargana
+    //4.tuswiig tootsoolno
+    //5.etssiin vldegdel tootsoog delgetsend gargana
+  };
 
-  document.addEventListener("keypress", function (event) {
-    if (event.key === 13 || event.which === 13) {
+  var setupEventListeners = function () {
+    var DOM = uiController.getDOMstrings();
+
+    document.querySelector(DOM.addBtn).addEventListener("click", function () {
       ctrlAddItem();
-    }
-  });
+    });
+
+    document.addEventListener("keypress", function (event) {
+      if (event.key === 13 || event.which === 13) {
+        ctrlAddItem();
+      }
+    });
+  };
+
+  return {
+    init: function () {
+      console.log("App started ...");
+      setupEventListeners();
+    },
+  };
 })(uiController, financeController);
+
+appController.init();
